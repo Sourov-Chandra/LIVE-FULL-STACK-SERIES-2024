58bbb801da2cd6044c27f852ed51a7d565787483
Sourov-Chandra <sourovtpi@yahoo.com>
2024-09-09 11:23:22 +0600

    JS part-1 Basic level

---
+++ lectures/javascript/learning.md
@@ -0,0 +1,20 @@
+# JS Topics
+
+## Basic JS
+
+- Introduction to JS
+- What is js?
+- what are the ways to add js?
+- setting up the environment
+- output & comment: alert(), confirm(), console
+- comment : single line comment, multiple line comments
+- tokens
+    - keywords,
+    - puncuators,
+    - backslash characters => \n, \t,-
+    - variables : var vs let vs const
+    - data types (typeof) => number, string, boolean, null, undefined, object, symbol
+    - string,
+    - truthy vs falsy values,
+    - operators
+- control statement
---
+++ lectures/javascript/script.js
@@ -0,0 +1,10 @@
+// Create a calculator
+
+const firstNumber = Number(prompt('Enter first number: '));
+const secondNumber = Number(prompt("Enter second number: "));
+
+console.log(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
+console.log(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
+console.log(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
+console.log(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
+console.log(`${firstNumber} % ${secondNumber} = ${firstNumber % secondNumber}`);
