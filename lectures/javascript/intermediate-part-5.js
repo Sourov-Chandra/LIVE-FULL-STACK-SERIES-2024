c64febff03de426044be339da4f3e90292d27b89
sourov-chandra <sourovtpi@yahoo.com>
2024-09-28 18:13:14 +0600

    JS part-5 intermediate level

---
+++ lectures/javascript/intermediate-part-5.js
@@ -0,0 +1,147 @@
+// import { add, sub, mul, div } from './calculator.js';
+// import {person} from './person.js';
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(1, 2));
+// console.log(div(1, 2));
+
+// console.log(person.printInfo());
+
+// Concept: rest parameters
+// const addNumbers = (message,...numbers) => {
+//   let sum;
+//   for (const number of numbers) {
+//     sum += number;
+//   }
+//   return sum;
+// };
+// console.log(addNumbers('hello', 10, 20));
+
+// Concept: spread operator (array)
+// const numbers = [10, 20, 30, 40, 50];
+// console.log([...numbers, 100, 200]);
+
+// Concept: spread operator (object)
+// const person = {
+//   name: 'John',
+//   age: 20,
+// };
+// console.log({...person, nationality: 'Bangladeshi'});
+
+// Concept: Destructure (object)
+// const person1 = {
+//     fullName: 'Sourov',
+//     age: 20,
+//     gender:'male'
+// }
+// console.log(person1.name);
+// console.log(person1.age);
+// console.log(person1.gender);
+
+// destructuring
+// const {fullName: f_name, age, gender} = person1;
+
+// console.log(f_name);  //destructuring and renaming
+// console.log(age);
+// console.log(gender);
+
+// const person2 = {
+//     name: 'John',
+//     age: 20,
+//     gender:'male'
+// }
+
+// const color = ['red', 'green', 'blue'];
+// // console.log(color[0]);
+// // console.log(color[1]);
+// // console.log(color[2]);
+
+// const [first, second, third] = color; //destructuring
+// console.log(first);
+// console.log(second);
+// console.log(third);
+
+// Concept: default perameters
+// const greet = (msg = 'Good day to you.') => {
+//     console.log(msg)
+// }
+// greet('good morning');
+// greet('good afternoon');
+// greet();
+// import { add, sub, mul, div } from './calculator.js';
+// import {person} from './person.js';
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(1, 2));
+// console.log(div(1, 2));
+
+// console.log(person.printInfo());
+
+
+// Concept: rest parameters
+// const addNumbers = (message,...numbers) => {
+//   let sum;
+//   for (const number of numbers) {
+//     sum += number;
+//   }
+//   return sum;
+// };
+// console.log(addNumbers('hello', 10, 20));
+
+
+
+ // Concept: spread operator (array)
+// const numbers = [10, 20, 30, 40, 50];
+// console.log([...numbers, 100, 200]);    
+
+
+// Concept: spread operator (object)
+// const person = {
+//   name: 'John',
+//   age: 20,
+// };
+// console.log({...person, nationality: 'Bangladeshi'});
+
+
+// Concept: Destructure (object)
+// const person1 = {
+//     fullName: 'Sourov',
+//     age: 20,
+//     gender:'male'
+// }
+// console.log(person1.name);
+// console.log(person1.age);
+// console.log(person1.gender);
+
+// destructuring
+// const {fullName: f_name, age, gender} = person1;
+
+// console.log(f_name);  //destructuring and renaming
+// console.log(age);
+// console.log(gender);
+
+// const person2 = {
+//     name: 'John',
+//     age: 20,
+//     gender:'male'
+// }
+
+// const color = ['red', 'green', 'blue'];
+// // console.log(color[0]);
+// // console.log(color[1]);
+// // console.log(color[2]);
+
+// const [first, second, third] = color; //destructuring
+// console.log(first);
+// console.log(second);
+// console.log(third);
+
+// Concept: default perameters
+// const greet = (msg = 'Good day to you.') => {
+//     console.log(msg)
+// }
+// greet('good morning');
+// greet('good afternoon');
+// greet();
--- lectures/javascript/learning.md
+++ lectures/javascript/learning.md
@@ -1,12 +1,12 @@
 # JS Topics
 
-## Basic JS
+## JavaScript part-1 (Basic)
 
 - Introduction to JS
 - What is js?
 - what are the ways to add js?
 - setting up the environment
-- output & comment: alert(), confirm(), console
+- output : alert(), confirm(), console
 - comment : single line comment, multiple line comments
 - tokens
     - keywords,
@@ -14,7 +14,52 @@
     - backslash characters => \n, \t,-
     - variables : var vs let vs const
     - data types (typeof) => number, string, boolean, null, undefined, object, symbol
-    - string,
     - truthy vs falsy values,
-    - operators
+
+## JavaScript part-2 (Basic)
+
+    - operators : Arithmetic(+, -, \*, /, %), Assignment, Comparision (<, >, <=, >=, ==, ===), Logical (&& || !) Unary
+
+## JavaScript part-3 (Basic)
+
 - control statement
+    - conditional control statement: if, else, else if, switch
+    - loop control statement
+        - for, while, do while
+        - while vs do while loop
+        - break, continue
+- Array, String, Object
+    - for of, for in
+
+## JavaScript part-4 (Basic)
+- function vs method
+    - function (traditional vs arrow)
+    - Method - Number, Math, Date, Array, String methods
+    - callback and higher order functions
+
+## JavaScript part-5(Intermediate)
+
+- ES6, ES7
+    - template literals
+    - var vs let vs const
+    - arrow function
+    - module
+    - spread Operator, Rest parameters
+    - Destructure
+    - default parameters (export defalut,in the function parameter)
+
+## JavaScript part-6 (Intermediate)
+- higher order functions: map, filter, foreach, reduce, callback functions
+- Error Handling
+
+## JavaScript part-7 (Intermediate)
+
+- Acynchronous programming: async, await, then, catch, callback
+- api calling 
+- JSON
+
+## JavaScript part-8 (Intermediate)
+- DOM and Event handler
+
+## JavaScript part-9 (Intermediate)
+- Project: Ecommece front-page
